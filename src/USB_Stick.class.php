98e6c19377826bbd96aa2937a326cb4a97414121
mreilaender <mreilaender@student.tgm.ac.at>
2015-01-14 17:35:35 +0100

    Ground Structur

---
+++ src/USB_Stick.class.php
@@ -0,0 +1,7 @@
+<?php 
+class USB_Stick extends ComputerProdukt {
+	public function __construct($nummer=1, $bezeichnung="USB_16_GB", $preis=9.99,$menge=1,$einheit=Einheiten::STK) {
+		parent::__construct($nummer, $bezeichnung,$preis,$menge,$einheit);
+	}
+}
+?>
