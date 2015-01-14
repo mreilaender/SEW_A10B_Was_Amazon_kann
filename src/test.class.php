b60c4531af00439bbaa0ac14bcacfc79feaf75e9
mreilaender <mreilaender@student.tgm.ac.at>
2015-01-14 17:57:39 +0100

    Tested some Testcases

---
+++ src/test.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/* Damit alle Errors angezeigt werden */
+ini_set ( 'display_errors', TRUE );
+ini_set ( 'display_startup_errors', TRUE );
+error_reporting ( E_ALL );
+function __autoload($class_name) {
+	include $class_name . '.class.php';
+}
+$fabrik = new TKFabrik();
+$fabrik->add($fabrik->erzeuge("TK-Spinat"));
+$fabrik->add($fabrik->erzeuge("TK-Maronireis"));
+$fabrik->add($fabrik->erzeuge("TK-Spinat"));
+
+$fabrik2 = new ElektronikFabrik();
+$fabrik2->add($fabrik2->erzeuge("USB_Stick"));
+
+$fabrik2->bezahlen();
+$fabrik2->verpacken();
+$fabrik2->verschicken();
+
+$fabrik->bezahlen();
+$fabrik->verpacken();
+$fabrik->verschicken();
+
+$fabrik->zeigeEinkaufswagen();
+$fabrik2->zeigeEinkaufswagen();
+?>
