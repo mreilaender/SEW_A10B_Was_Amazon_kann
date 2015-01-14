946e783a5192578af78f45c90c24c4f254aed6bc
mreilaender <mreilaender@student.tgm.ac.at>
2015-01-14 16:49:38 +0100

    Ground Structure, added all constants

---
+++ src/Einheiten.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Beinhaltet alle Einheiten als konstanten
+ * @author Manuel Reiländer
+ *
+ */
+class Einheiten{
+	const STK="stk";
+	const M="m";
+	const KG="kg";
+	const L="l";
+	const G="g";
+	const ML="ml";
+}
+?>
