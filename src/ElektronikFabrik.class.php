9fc81660a05e49083dfd6bbaf85500b7db8e3dc4
mreilaender <mreilaender@student.tgm.ac.at>
2015-01-14 18:44:02 +0100

    Added new scenario for new products

--- src/ElektronikFabrik.class.php
+++ src/ElektronikFabrik.class.php
@@ -7,11 +7,11 @@ class ElektronikFabrik extends Fabrik {
 		if (strcmp($produkt, "USB_Stick") == 0) {
 			return new USB_Stick ( 1, "USB_16_GB", 9.99 );
 		}
-		if (strcmp($produkt, "USB-Computertastatur_102Tasten")) {
-			// TODO
+		if (strcmp($produkt, "USB-Computertastatur 102 Tasten")) {
+			return new USB_Computertastatur_102Tasten();
 		}
-		if (strcmp($produkt, "USB-Computermaus_laserabgetastet")) {
-			// TODO
+		if (strcmp($produkt, "USB-Computermaus laserabgetastet")) {
+			return new USB-Computermaus_laser();
 		}
 		return $tmp;
 	}
